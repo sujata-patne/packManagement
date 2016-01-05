973ece86c2fac30906343eb585714cde37ab4605
nishtha-gupta <nishta1992@gmail.com>
2016-01-05 10:50:05 +0530

    minor changes

--- public/scripts/controller/arrangeContentList.js
+++ public/scripts/controller/arrangeContentList.js
@@ -22,6 +22,7 @@ myApp.controller('arrangeContentListCtrl', function ($scope, $window,thumb_path,
     $scope.thumb_path = thumb_path;
 
 
+
     Search.getSavedContents({pctId:$scope.pctId}, function (data) {
         $scope.searchContentList = angular.copy(data.contents);
         $scope.sequence = angular.copy(data.packContentsSequence);
@@ -44,129 +45,156 @@ myApp.controller('arrangeContentListCtrl', function ($scope, $window,thumb_path,
     });
 
     $scope.checkForDuplicates = function(id) {
-    	var unique = [];
-    	var duplicate = [];
-    	var previous_value = 0;
+        var unique = [];
+        var duplicate = [];
+        var previous_value = 0;
         angular.forEach($scope.sequence,function(value,key) {
-        	if( unique.length == 0 ) {
+            if( unique.length == 0 ) {
                 unique.push( parseInt( value ) );
             } else if( unique.indexOf(parseInt( value ) ) == -1 ) {
                 unique.push( parseInt( value ) );
             } else {
-            	duplicate.push( parseInt( value ) );
+                duplicate.push( parseInt( value ) );
             }
-        	$scope.unique = unique;
-        	$scope.duplicate = duplicate;
+            $scope.unique = unique;
+            $scope.duplicate = duplicate;
         });
         if( $scope.duplicate.length > 0 ) {
-        	toastr.error("Duplicate values not allowed!");
-        	$scope.sequence[id] = "";
+            toastr.error("Duplicate values not allowed!");
+            $scope.sequence[id] = "";
         }
     }
-    
+
+
     $scope.arrangeContent = function () {
         $scope.arrangedContentList = {};
+        $scope.flag = false;
         angular.forEach($scope.sequence,function(value,key) {
             var data = {};
-            data[key] = value;
-            $scope.arrangedContentList[key] = value;
+            console.log(value);
+            if( angular.isUndefined(value)){
+                $scope.flag = true;
+            }else{
+                data[key] = value;
+                $scope.arrangedContentList[key] = value;
+            }
+
         })
     }
+
     $scope.addMoreSearchContents = function () {
+
         $scope.arrangeContent();
-        console.log('test '+$scope.pctId)
-        arrangeContents.saveArrangedContents({pctId:$scope.pctId, packId:$scope.packId, arrangedContentList:$scope.arrangedContentList}, function (data) {
-            //$window.location.href = "/#/search-content/"+$scope.pctId;
-            
-            var filename = '';
-            if($scope.displayName == 'Rule Based'){
-                filename = 'search-content-rule';
-            }else{
-                filename = 'search-content-'+$scope.displayName.toLowerCase();
-            }   
-            $state.go(filename, {pctId:$scope.pctId})
-            toastr.success(data.message)
-        },function(error){
-            console.log(error)
-            toastr.error(error)
-        })
+        if( $scope.flag == true  ) {
+            toastr.error("Enter a number in range 1 to 9999");
+        } else{
+            console.log('test '+$scope.pctId)
+            arrangeContents.saveArrangedContents({pctId:$scope.pctId, packId:$scope.packId, arrangedContentList:$scope.arrangedContentList}, function (data) {
+                //$window.location.href = "/#/search-content/"+$scope.pctId;
+
+                var filename = '';
+                if($scope.displayName == 'Rule Based'){
+                    filename = 'search-content-rule';
+                }else{
+                    filename = 'search-content-'+$scope.displayName.toLowerCase();
+                }
+                $state.go(filename, {pctId:$scope.pctId})
+                toastr.success(data.message)
+            },function(error){
+                console.log(error)
+                toastr.error(error)
+            });
+        }
     }
 
     $scope.savePublishedContents = function () {
-        $scope.arrangeContent(); 
-        arrangeContents.savePublishedContents({pctId:$scope.pctId, packId:$scope.packId, arrangedContentList:$scope.arrangedContentList}, function (data) {
-            toastr.success(data.message)
-        },function(error){
-            console.log(error)
-            toastr.error(error)
-        })
+
+        $scope.arrangeContent();
+        if( $scope.flag == true  ) {
+            toastr.error("Enter a number in range 1 to 9999");
+        } else{
+            arrangeContents.savePublishedContents({pctId:$scope.pctId, packId:$scope.packId, arrangedContentList:$scope.arrangedContentList}, function (data) {
+                toastr.success(data.message)
+            },function(error){
+                console.log(error)
+                toastr.error(error)
+            });
+        }
     }
 
+
+
     $scope.saveArrangedContents = function(){
+
         $scope.arrangeContent();
-        arrangeContents.saveArrangedContents({pctId:$scope.pctId, packId:$scope.packId, arrangedContentList:$scope.arrangedContentList}, function (data) {
-            if( data.error === true ) {
-            	toastr.error(data.message);
-            }else {
-            	toastr.success(data.message);
-            }
-        },function(error){
-            console.log(error)
-            toastr.error(error)
-        })
+        if( $scope.flag == true  ) {
+            toastr.error("Enter a number in range 1 to 9999");
+        } else{
+            arrangeContents.saveArrangedContents({pctId:$scope.pctId, packId:$scope.packId, arrangedContentList:$scope.arrangedContentList}, function (data) {
+                if( data.error === true ) {
+                    toastr.error(data.message);
+                }else {
+                    toastr.success(data.message);
+                }
+            },function(error){
+                console.log(error)
+                toastr.error(error)
+            });
+        }
     }
+
 
     $(".progress").hide();
 
 
     $scope.fileUploads = [];
     $scope.uploadSubmit = function(index,cm_id){
-             $scope.files = $scope.fileUploads;
-             var valid = true;
-             if($scope.files.length == 0){
-                valid = false;
-                toastr.error("Please select images to upload.");
-             }
-             if($scope.files[index].length > 2){
-                valid = false;
-                toastr.error("Max 2 files allowed per content type");
-             }
+        $scope.files = $scope.fileUploads;
+        var valid = true;
+        if($scope.files.length == 0){
+            valid = false;
+            toastr.error("Please select images to upload.");
+        }
+        if($scope.files[index].length > 2){
+            valid = false;
+            toastr.error("Max 2 files allowed per content type");
+        }
 
-             if(valid){
-                      angular.forEach($scope.files[index], function(file) {
-                        if (file && !file.$error) {
-                            file.upload = Upload.upload({
-                              url: '/UploadFile',
-                              fields: {'cm_id': cm_id},
-                              file: file
-                            });
+        if(valid){
+            angular.forEach($scope.files[index], function(file) {
+                if (file && !file.$error) {
+                    file.upload = Upload.upload({
+                        url: '/UploadFile',
+                        fields: {'cm_id': cm_id},
+                        file: file
+                    });
 
-                            file.upload.then(function (response) {
-                              $timeout(function () {
-                                file.result = response.data;
-                              });
-                            }, function (response) {
-                              if (response.status > 0)
-                                $scope.errorMsg = response.status + ': ' + response.data;
-                            });
+                    file.upload.then(function (response) {
+                        $timeout(function () {
+                            file.result = response.data;
+                        });
+                    }, function (response) {
+                        if (response.status > 0)
+                            $scope.errorMsg = response.status + ': ' + response.data;
+                    });
 
-                            file.upload.progress(function (evt) {
-                                var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                                $("#"+index).html("Uploaded: "+progressPercentage+"%");
-                                $scope.fileUploads[index].progress = Math.min(100, parseInt(100.0 * 
-                                                       evt.loaded / evt.total));
+                    file.upload.progress(function (evt) {
+                        var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                        $("#"+index).html("Uploaded: "+progressPercentage+"%");
+                        $scope.fileUploads[index].progress = Math.min(100, parseInt(100.0 *
+                            evt.loaded / evt.total));
 
-                                if( progressPercentage == 100 ){
-                                    setTimeout(function(){
-                                        window.location.reload();
+                        if( progressPercentage == 100 ){
+                            setTimeout(function(){
+                                window.location.reload();
 
-                                    },1000);
-                                }
-                            });
-                        }   
-                     }); 
-             }
-           
+                            },1000);
+                        }
+                    });
+                }
+            });
+        }
+
     }
 
     $scope.backToAddContentList = function(){
@@ -178,12 +206,16 @@ myApp.controller('arrangeContentListCtrl', function ($scope, $window,thumb_path,
         $("a.grouped_elements").fancybox({
             'transitionIn'  :   'elastic',
             'transitionOut' :   'elastic',
-            'speedIn'       :   600, 
-            'speedOut'      :   200, 
+            'speedIn'       :   600,
+            'speedOut'      :   200,
             'maxWidth'      :   400,
             'maxHeight'     :   400,
             'modal'         : false,
             'closeBtn'      : true
         });
     });
+    $scope.isNumber = function(e) {
+        var key = e.keyCode ? e.keyCode : e.which;
+        if( (isNaN(String.fromCharCode(key)) && key !=8 )||key == 32) e.preventDefault();
+    }
 })
